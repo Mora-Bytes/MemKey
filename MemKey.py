8fe528b3c9b797756f1080bfeeb9543cb71dd3bc
Mora <MoraDev@proton.me>
2023-07-07 07:18:06 +0000

    Update MemKey.py to 0.2
    Timeouts after 75 rounds(Not Customizable).
    Fixed the Length system(Customizable).
    Checks how Strong a Password is.

--- MemKey.py
+++ MemKey.py
@@ -1,16 +1,22 @@
+#MoraByte2027 - MemKey/0.2 (Windows nt 10; x64)
+
 try:
 	import pyhibp, random
 	import string, os
 	import platform, sys
-	import hashlib
+	import hashlib, re
 
 	from pyhibp import pwnedpasswords as pw
 
 	size = 10
 	if len(sys.argv) >= 2:
 		size = int(sys.argv[1])+2
+		if size <= 2:
+			size = 6
 
-	user = os.getlogin()
+	time = 0
+
+	user      = os.getlogin()
 	is_64bits = sys.maxsize > 2**32
 
 	if is_64bits: bit = 'x64'
@@ -22,7 +28,7 @@ try:
 
 	Creator = """MoraByte2027"""
 
-	Ver = '0.1'
+	Ver = '0.2'
 
 	agent = "{0} - MemKey/{1} {2}".format(Creator, Ver, System)
 
@@ -30,9 +36,9 @@ try:
 
 	pyhibp.set_user_agent(ua=agent)
 
-	paswrd = ''
+	paswrd   = ''
 	password = ''
-	done = False
+	done     = False
 
 	while not done:
 		for i in range(random.randint(3, 6)):
@@ -49,7 +55,14 @@ try:
 		password = hex(int(hashlib.sha256(str(password).encode(), usedforsecurity=True).hexdigest(),16) + password)[2:size]
 
 		resp = pw.is_password_breached(password=password)
-		if not resp:
-			print("Password: {0}".format(password))
+		ok   = not ((re.search(r"[a-z]", password) is None) or (re.search(r"\d", password) is None) or (len(password) <= 11))
+		if ok and not resp:
+			print("Password: {0} (Strong)".format(password))
 			done = True
+		elif not resp:
+			print("Password: {0} (Weak)".format(password))
+			done = True
+		else:
+			time += 1
+		if time >= 75: raise TimeoutError('Please up the Password Length.')
 except KeyboardInterrupt: pass
